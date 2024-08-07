48cb3ce4dcb022c1e3dbcfb11d9b551425011ca4
kewon96 <yang0184@naver.com>
2024-08-07 13:30:40 +0900

    initial commit

---
+++ src/main/java/sh/kr/spring_modulith_example/SpringModulithExampleApplication.java
@@ -0,0 +1,13 @@
+package sh.kr.spring_modulith_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulithExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringModulithExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sh/kr/spring_modulith_example/SpringModulithExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.kr.spring_modulith_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulithExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
